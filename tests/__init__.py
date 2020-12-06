94b49f9546b7957b9cdca91b8be4e42b30987137
Guillaume Ayoub <guillaume@courtbouillon.org>
2020-12-06 12:14:13 +0100

    Clean tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 """
-Test suite for pydyf
+Test suite for pydyf.
 
 This module adds a PNG export based on GhostScript. As GhostScript is released
 under AGPL, the whole testing suite is released under AGPL.
@@ -8,6 +8,7 @@ under AGPL, the whole testing suite is released under AGPL.
 
 import io
 import os
+from pathlib import Path
 from subprocess import PIPE, run
 
 from PIL import Image
@@ -18,73 +19,59 @@ PIXELS_BY_CHAR = dict(
     B=(0, 0, 255),  # blue
     G=(0, 255, 0),  # lime green
     K=(0, 0, 0),  # black
-    z=None,
+    z=None,  # any color
 )
 
 
-def document_to_png(document, resolution=72, antialiasing=1):
-    output = io.BytesIO()
-    document.write(output)
-    # TODO: don’t crash if GhostScript can’t be found
-    # TODO: fix that for Windows
-    command = [
-        'gs', '-q',
-        '-sstdout=%%stderr' if os.name == 'nt' else '-sstdout=%stderr',
-        '-dNOPAUSE', '-dSAFER', f'-dTextAlphaBits={antialiasing}',
-        f'-dGraphicsAlphaBits={antialiasing}', '-sDEVICE=png16m',
-        f'-r{resolution * 8}', '-dDownScaleFactor=8', '-sOutputFile=-', '-']
-    command = run(command, input=output.getvalue(), stdout=PIPE)
-    return command.stdout
-
-
-def parse_pixels(pixels, pixels_overrides=None):
-    chars = dict(PIXELS_BY_CHAR, **(pixels_overrides or {}))
-    lines = tuple(line.split('#')[0].strip() for line in pixels.splitlines())
-    lines = tuple(line for line in lines if line)
-    width, height = len(lines[0]), len(lines)
-    pixels = tuple(chars[char] for line in lines for char in line)
-    return pixels, width, height
-
-
 def assert_pixels(document, reference_pixels):
-    """Helper testing the size of the image and the pixels values."""
-    png = document_to_png(document)
-    reference_pixels, width, height = parse_pixels(reference_pixels)
+    """Test that the rendered document matches the reference pixels."""
+
+    # Transform the PDF document into a list of RGB tuples
+    pdf = io.BytesIO()
+    document.write(pdf)
+    stderr = '%%stderr' if os.name == 'nt' else '%stderr'
+    command = [
+        'gs', '-q', f'-sstdout={stderr}', '-dNOPAUSE', '-sDEVICE=png16m',
+        '-dSAFER', '-r576', '-dDownScaleFactor=8', '-sOutputFile=-', '-']
+    png = run(command, input=pdf.getvalue(), stdout=PIPE).stdout
     image = Image.open(io.BytesIO(png))
+    pixels = image.getdata()
+
+    # Transform reference drawings into a list of RGB tuples
+    lines = tuple(
+        line.strip() for line in reference_pixels.splitlines() if line.strip())
+    assert len({len(line) for line in lines}) == 1, (
+        'The lines of reference pixels don’t have the same length')
+    width, height = len(lines[0]), len(lines)
     assert (width, height) == image.size, (
         f'Reference size is {width}×{height}, '
         f'output size is {image.width}×{image.height}')
-    pixels = image.getdata()
-    assert_pixels_equal(width, height, pixels, reference_pixels)
+    reference_pixels = tuple(
+        PIXELS_BY_CHAR[char] for line in lines for char in line)
 
-
-def write_png(basename, pixels, width, height):  # pragma: no cover
-    """Take a pixel matrix and write a PNG file."""
-    directory = os.path.join(os.path.dirname(__file__), 'results')
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
-    filename = os.path.join(directory, basename + '.png')
-    image = Image.new('RGB', (width, height))
-    image.putdata(pixels)
-    image.save(filename)
-
-
-def assert_pixels_equal(width, height, raw, reference_raw, tolerance=0):
-    """Take 2 matrices of pixels and assert that they are the same."""
-    if raw != reference_raw:  # pragma: no cover
-        for i, (value, reference) in enumerate(zip(raw, reference_raw)):
+    # Compare pixels
+    if pixels != reference_pixels:  # pragma: no cover
+        for i, (value, reference) in enumerate(zip(pixels, reference_pixels)):
             if reference is None:
                 continue
-            if any(abs(value - reference) > tolerance
+            if any(value != reference
                    for value, reference in zip(value, reference)):
                 name = os.environ.get('PYTEST_CURRENT_TEST')
                 name = name.split(':')[-1].split(' ')[0]
-                write_png(name, raw, width, height)
-                reference_raw = [
-                    pixel or (255, 255, 255) for pixel in reference_raw]
-                write_png(name + '.reference', reference_raw, width, height)
-                x = i // width
-                y = i % width
+                write_png(f'{name}', pixels, width, height)
+                reference_pixels = [
+                    pixel or (255, 255, 255) for pixel in reference_pixels]
+                write_png(f'{name}-reference', reference_pixels, width, height)
+                x, y = i // width, i % width
                 assert 0, (
                     f'Pixel ({x}, {y}) in {name}: '
                     f'reference rgba{reference}, got rgba{value}')
+
+
+def write_png(name, pixels, width, height):  # pragma: no cover
+    """Take a pixel matrix and write a PNG file."""
+    directory = Path(__file__).parent / 'results'
+    directory.mkdir(exist_ok=True)
+    image = Image.new('RGB', (width, height))
+    image.putdata(pixels)
+    image.save(directory / f'{name}.png')
