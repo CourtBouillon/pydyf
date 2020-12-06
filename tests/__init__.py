3e05491c91de3ffa88ba9c6c47756c0e7016194f
Guillaume Ayoub <guillaume@courtbouillon.org>
2020-12-06 01:20:22 +0100

    Simplify PDF to PNG transformation

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,6 @@ under AGPL, the whole testing suite is released under AGPL.
 
 import io
 import os
-import shutil
 from subprocess import PIPE, run
 
 from PIL import Image
@@ -23,7 +22,7 @@ PIXELS_BY_CHAR = dict(
 )
 
 
-def document_to_png(document, target=None, resolution=72, antialiasing=1):
+def document_to_png(document, resolution=72, antialiasing=1):
     output = io.BytesIO()
     document.write(output)
     # TODO: don’t crash if GhostScript can’t be found
@@ -35,40 +34,7 @@ def document_to_png(document, target=None, resolution=72, antialiasing=1):
         f'-dGraphicsAlphaBits={antialiasing}', '-sDEVICE=png16m',
         f'-r{resolution * 8}', '-dDownScaleFactor=8', '-sOutputFile=-', '-']
     command = run(command, input=output.getvalue(), stdout=PIPE)
-    pngs = command.stdout
-    magic_number = b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
-
-    # TODO: use a different way to find PNG files in stream
-    if pngs.count(magic_number) == 1:
-        if target is None:
-            return pngs
-        png = io.BytesIO(pngs)
-    else:
-        images = []
-        for i, png in enumerate(pngs[8:].split(magic_number)):
-            images.append(Image.open(io.BytesIO(magic_number + png)))
-
-        width = max(image.width for image in images)
-        height = sum(image.height for image in images)
-        output_image = Image.new('RGBA', (width, height))
-        top = 0
-        for image in images:
-            output_image.paste(
-                image, (int((width - image.width) / 2), top))
-            top += image.height
-        png = io.BytesIO()
-        output_image.save(png, format='png')
-
-    png.seek(0)
-
-    if target is None:
-        return png.read()
-
-    if hasattr(target, 'write'):
-        shutil.copyfileobj(png, target)
-    else:
-        with open(target, 'wb') as fd:
-            shutil.copyfileobj(png, fd)
+    return command.stdout
 
 
 def parse_pixels(pixels, pixels_overrides=None):
